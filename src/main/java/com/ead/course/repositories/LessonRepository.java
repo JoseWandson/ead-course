bfefa72adc04a4d3f22523340255bfff524d9c84
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2022-06-13 18:27:55 -0300

    Implementação de deleção em cascata

--- src/main/java/com/ead/course/repositories/LessonRepository.java
+++ src/main/java/com/ead/course/repositories/LessonRepository.java
@@ -2,8 +2,13 @@ package com.ead.course.repositories;
 
 import com.ead.course.models.LessonModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface LessonRepository extends JpaRepository<LessonModel, UUID> {
+
+    @Query("select l from LessonModel l where l.module.moduleId = :moduleId")
+    List<LessonModel> findAllLessonsIntoModule(UUID moduleId);
 }
