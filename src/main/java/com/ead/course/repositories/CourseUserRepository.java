3de4cc889cac13409fc38f3d2ee57c2b8be5f99b
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2023-02-22 14:21:10 -0300

    Comunicação Síncrona - Preparação de Deleção Entre Microservices

--- src/main/java/com/ead/course/repositories/CourseUserRepository.java
+++ src/main/java/com/ead/course/repositories/CourseUserRepository.java
@@ -3,10 +3,15 @@ package com.ead.course.repositories;
 import com.ead.course.models.CourseModel;
 import com.ead.course.models.CourseUserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CourseUserRepository extends JpaRepository<CourseUserModel, UUID> {
 
     boolean existsByCourseAndUserId(CourseModel courseModel, UUID userId);
+
+    @Query("select c from CourseUserModel c where c.course.courseId = :courseId")
+    List<CourseUserModel> findAllCourseUserIntoCourse(UUID courseId);
 }
