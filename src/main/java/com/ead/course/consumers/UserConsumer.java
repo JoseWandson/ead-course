a3cb7c49f6ee7c6530176d61bc5522543d5cb592
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2023-12-28 15:24:42 -0300

    Criar Consumer e Receber Event State Tranfer

---
+++ src/main/java/com/ead/course/consumers/UserConsumer.java
@@ -0,0 +1,32 @@
+package com.ead.course.consumers;
+
+import com.ead.course.dtos.UserEventDto;
+import com.ead.course.enums.ActionType;
+import com.ead.course.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserConsumer {
+
+    private final UserService userService;
+
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(value = "${ead.broker.queue.userEventQueue.name}", durable = "true"),
+            exchange = @Exchange(value = "${ead.broker.exchange.userEventExchange}", type = ExchangeTypes.FANOUT, ignoreDeclarationExceptions = "true"))
+    )
+    public void listenUserEvent(@Payload UserEventDto userEventDto) {
+        var userModel = userEventDto.convertToUserModel();
+
+        if (ActionType.valueOf(userEventDto.getActionType()) == ActionType.CREATE) {
+            userService.save(userModel);
+        }
+    }
+}
